b8de3da3685f875205f8e7265720b0f79c65ebc6
towhidulislamalif <towhidulislamalif@gmail.com>
2023-03-09 22:38:57 +0600

    server connect

--- client/app.js
+++ client/app.js
@@ -76,7 +76,7 @@ async function handleSubmit(event) {
 
   try {
     // Send a POST request to the server
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-3uvv.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
